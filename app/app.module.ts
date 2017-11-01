58639bc6bfc3b74a110306ff4d52e2116ac04364
Vladimir Ivanov <ivanovnetua@gmail.com>
2017-11-01 16:08:40 +0200

    Fix rename app and add custom component

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
+import { HelloWorldComponent } from "./hello-world-list/hello-world-list.component";
 
 @NgModule({
     imports: [BrowserModule],
-    declarations: [AppComponent],
+    declarations: [AppComponent, HelloWorldComponent],
     bootstrap: [AppComponent]
 })
 export class AppModule {
---
+++ app/hello-world-list/phrases.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Phrase = /** @class */ (function () {
+    function Phrase() {
+    }
+    return Phrase;
+}());
+exports.Phrase = Phrase;
+//# sourceMappingURL=phrases.js.map
